bcef95a7b884795a2270804edc7b981584b5f1e8
Brian Cairl <briancairl@gmail.com>
2019-11-25 23:10:16 -0500

    Updates guards/documentation in support.h

--- include/support.h
+++ include/support.h
@@ -1,10 +1,31 @@
 #ifndef MMPL_SUPPORT_H
 #define MMPL_SUPPORT_H
 
-#include <cassert>
+#ifdef NDEBUG
 
+#define MMPL_RUNTIME_ASSERT(expr)
+
+#else  // NDEBUG
+
+/**
+ * @brief Wraps runtime assert to mask out durin non-debug builds
+ */
+#include <cassert>
 #define MMPL_RUNTIME_ASSERT(expr) assert(expr)
 
-#define MMPL_STATIC_ASSERT_MSG(msg) "[\n" msg "\n]"
+#endif  // NDEBUG
+
+
+#ifndef MMPL_DISABLE_CLEARER_STATIC_ASSERT_MESSAGES
+/**
+ * @brief Wraps static_assert message to make it easier to find in build errors
+ */
+#define MMPL_STATIC_ASSERT_MSG(msg) "[\n\n" msg "\n\n]"
+
+#else  // MMPL_DISABLE_CLEARER_STATIC_ASSERT_MESSAGES
+
+#define MMPL_STATIC_ASSERT_MSG(msg) msg
+
+#endif  // MMPL_DISABLE_CLEARER_STATIC_ASSERT_MESSAGES
 
 #endif  // MMPL_SUPPORT_H
